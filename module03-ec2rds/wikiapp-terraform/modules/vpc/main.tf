3fe90d9dacbdf55ab824dfc829a6112284048ea5
Romel <43550213+rpc180@users.noreply.github.com>
2025-07-09 16:01:45 -0400

    Updated VPC with outbound internet gatway and route

--- module03-ec2rds/wikiapp-terraform/modules/vpc/main.tf
+++ module03-ec2rds/wikiapp-terraform/modules/vpc/main.tf
@@ -1,3 +1,26 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "wikiapp-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "wikiapp-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
