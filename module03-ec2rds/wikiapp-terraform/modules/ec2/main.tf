990e46de2f7fa17d5eb340900a0f2135164fe36b
Romel <43550213+rpc180@users.noreply.github.com>
2025-07-10 16:31:02 -0400

    Added SSH rule to EC2 from cloudshell

--- module03-ec2rds/wikiapp-terraform/modules/ec2/main.tf
+++ module03-ec2rds/wikiapp-terraform/modules/ec2/main.tf
@@ -17,6 +17,14 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH from cloudshell"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
