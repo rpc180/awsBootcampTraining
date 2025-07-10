db63ba3e99608ed1f21c97492bd0186b4a3410bf
Romel <43550213+rpc180@users.noreply.github.com>
2025-07-10 14:07:29 -0400

    Cleared output values from root main.tf as an outputs.tf exists there.

--- module03-ec2rds/wikiapp-terraform/main.tf
+++ module03-ec2rds/wikiapp-terraform/main.tf
@@ -29,11 +29,3 @@ module "rds" {
   db_password       = var.db_password
   db_name           = var.db_name
 }
-
-output "ec2_public_ip" {
-  value = module.ec2.public_ip
-}
-
-output "rds_endpoint" {
-  value = module.rds.endpoint
-}
