0a52acf4114c44d63c5210871aaa0e28afbe401e
Romel <43550213+rpc180@users.noreply.github.com>
2025-07-12 10:26:13 -0400

    Updated ingress for ec2 to 8080

--- module03-ec2rds/wikiapp-terraform/modules/ec2/main.tf
+++ module03-ec2rds/wikiapp-terraform/modules/ec2/main.tf
@@ -37,8 +37,8 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
