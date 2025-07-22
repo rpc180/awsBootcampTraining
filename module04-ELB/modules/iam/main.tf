1707333a1ae19d4d70bb09d4e1db3eeee8088346
Romel <43550213+rpc180@users.noreply.github.com>
2025-07-22 14:17:40 -0400

    Updated IAM policies to allow EC2 to connect to S3 for app package download (attempt 1)

--- module04-ELB/modules/iam/main.tf
+++ module04-ELB/modules/iam/main.tf
@@ -3,6 +3,23 @@ resource "aws_iam_role" "beanstalk_service_role" {
   assume_role_policy = data.aws_iam_policy_document.beanstalk_assume_role.json
 }
 
+resource "aws_iam_role" "ec2_instance_role" {
+  name = "eb_ec2_instance_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
 data "aws_iam_policy_document" "beanstalk_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -18,9 +35,19 @@ resource "aws_iam_role_policy_attachment" "attach_managed_policies" {
     "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker",
     "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier",
     "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier",
-    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkManagedUpdatesCustomerRolePolicy"
   ])
   role       = aws_iam_role.beanstalk_service_role.name
+  policy_arn = each.value
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_basic_policies" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier",
+    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  ])
+  role       = aws_iam_role.ec2_instance_role.name
   policy_arn = each.value
 }
 
@@ -29,5 +56,5 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
     create_before_destroy = true
   }
   name = "eb_ec2_instance_profile"
-  role = aws_iam_role.beanstalk_service_role.name
+  role = aws_iam_role.ec2_instance_role.name
 }
